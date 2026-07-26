81a00dec4f07ff881abaf82545603f665e1b3419
swp-wooki <59758107+swp-wooki@users.noreply.github.com>
2026-07-26 16:55:51 +0000

    Deploying to gh-pages from @ swp-wooki/swp-wooki.github.io@0762f1382a9d59c0db896ba53bcae4caa3f39efe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,13 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-finish-undergraduate-ra-program-for-analysis-and-pdes",
+          title: 'Finish Undergraduate RA program for Analysis and PDEs',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-entered-into-bs-ms-program-for-department-of-industrial-engineering-hanyang-university",
+          title: 'Entered into BS-MS program for Department of Industrial Engineering, Hanyang University.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
